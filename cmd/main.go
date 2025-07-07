391f7fda9620f3ea5947f50f0431b4688ea650f5
cami-crane <hatena.m.k.7@gmail.com>
2025-07-08 02:06:51 +0900

    hello_world の作成

---
+++ cmd/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/cami-crane/lesson-go.git/cmd/syntax"
+)
+
+// 最初に実行される関数
+func init() {
+	fmt.Println("init")
+}
+
+func main() {
+		syntax.HelloWorld()
+}
